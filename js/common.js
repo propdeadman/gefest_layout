e9cffb3e52328ab2e5d25a6a37e30f513553e419
propdeadman <walkeruilson@gmail.com>
2022-07-24 03:53:28 +0400

    layout-complete

--- js/common.js
+++ js/common.js
@@ -1,6 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-	// $('body').hide()
-
-})
-
+    function openbox(id, tt) {
+        var div = document.getElementById(id);
+        var tt_div = document.getElementById(tt);
+        if (div.style.display == 'block') {
+            div.style.display = 'none';
+        } else {
+            div.style.display = 'block';
+        }
+    }
